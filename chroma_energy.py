b763e4296e424872efc361dc81f6af249d2f0945
Manan <manansoni1399@gmail.com>
2022-01-11 10:58:23 +0000

    add led support to chroma energy

--- chroma_energy.py
+++ chroma_energy.py
@@ -6,8 +6,20 @@ from scipy.signal import savgol_filter
 import librosa
 import librosa.display
 from pprint import pprint
+from rpi_ws281x import PixelStrip, Color
+import time
 
-fig, ax = plt.subplots()
+# LED strip configuration:
+LED_COUNT = 60        # Number of LED pixels.
+LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
+# LED_PIN = 10        # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA = 10          # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 128  # Set to 0 for darkest and 255 for brightest
+LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
 
 MIC_RATE = 44100
 FPS = 30
@@ -22,13 +34,12 @@ cmap = cm.ScalarMappable(
 cval = lambda i,x: (x+i/60) if (x+i/60) < 1 else (x - 1 + i/60)
 
 def ledcolors(data):
+    global cmap, cval
     norm_data = np.mean(data, axis=1)
     ydata,cols = np.zeros(BINS), np.zeros(BINS)
     ydata = np.repeat(norm_data, BINS/norm_data.shape[0])
-    cols = [cval(i,ydata[i]) for i in range(BINS)]
+    cols = [cmap.to_rgba(cval(i,ydata[i]))[:3] for i in range(BINS)]
     return ydata, cols
-
-    
 
 def start_stream(callback):
     p = pyaudio.PyAudio()
@@ -57,19 +68,17 @@ def start_stream(callback):
     return overflows
 
 def audio_func(audio):
-    global count
+    global count, strip
     while audio.shape[0] > 0 and count <= TMAX:
         print(count)
         chroma = librosa.feature.chroma_stft(audio, sr=MIC_RATE)
-        plt.cla()
-        data, cols = ledcolors(chroma)
-        plt.scatter(range(BINS), data, c=cols)
-        plt.pause(0.01)
+        _, cols = ledcolors(chroma)
+        for i in range(BINS):
+            strip.setPixelColor(i, Color(*cols[i]))
         count += 1
         return False
     return True
 
 
 print('overflows', start_stream(audio_func))
-plt.show()
 
